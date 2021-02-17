7e6c55e82d672e7f73621898655a1d72fd25be77
shubhekshu <shubhekshushukla@gmail.com>
2021-02-17 17:09:50 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,15 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  version = "~> 3.0"
+  region  = "us-east-1"
 }
 
 # Create a VPC
